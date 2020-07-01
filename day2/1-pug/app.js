1bbd118751eb48f3bfd887146174114d62a9326a
Mehmet Seven <mehmetseven0@gmail.com>
2020-07-01 11:33:33 +0300

    pug public folder defination

--- day2/1-pug/app.js
+++ day2/1-pug/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -26,6 +27,10 @@ app.get('/', (req, res) => {
 			},
 		],
 	});
+});
+
+app.get('/contact', (req, res) => {
+	res.render('contact');
 });
 
 app.listen(3000, () => console.log('Server is up!'));
