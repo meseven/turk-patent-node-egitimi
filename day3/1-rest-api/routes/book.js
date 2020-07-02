d92a4faa42b202567869c326d1052c60c5ede692
Mehmet Seven <mehmetseven0@gmail.com>
2020-07-02 11:50:32 +0300

    findById

--- day3/1-rest-api/routes/book.js
+++ day3/1-rest-api/routes/book.js
@@ -3,9 +3,20 @@ const router = express.Router();
 
 // models
 const Book = require('../models/Book');
+const { route } = require('.');
 
-router.get('/', function (req, res, next) {
-	res.send('book');
+router.get('/get/:bookId', async (req, res) => {
+	try {
+		const book = await Book.findById(req.params.bookId);
+		res.json(book);
+	} catch {
+		res.json({ error: { message: 'Book was not found.', code: 1001 } });
+	}
+});
+
+router.get('/list', async (req, res, next) => {
+	const books = await Book.find({}).limit(30).collation({ locale: 'en' }).sort({ year: 1 });
+	res.json(books);
 });
 
 router.post('/', async (req, res) => {
